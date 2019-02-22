051c34b06b89c798f703dc41f252be63b26eec96
Érika <erikassdc1@gmail.com>
2019-02-22 09:26:40 -0300

    T

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,7 +21,7 @@ AUTHOR_FEED_RSS = None
 
 #Theme
 THEME='/home/matheus-os/Downloads/Themes/pelican-clean'
-#HEADER_COVER = '/home/matheus-os/Downloads/Themes/pelican-clean/static/images/home-bg.jpg'
+HEADER_COVER='/home/matheus-os/Downloads/Themes/pelican-clean/static/images/home-bg.jpg'
 
 DEFAULT_PAGINATION = 10
 
